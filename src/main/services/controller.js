d80443d69051514f79c77c7818670e8409b81c75
Luis Prado <lpradopostigo@gmail.com>
2022-04-08 08:24:36 -0500

    enable startup scanning

--- src/main/services/controller.js
+++ src/main/services/controller.js
@@ -1,8 +1,11 @@
 const log = require("loglevel");
+const fsp = require("fs/promises");
 const Database = require("./Database");
 const scanner = require("./scanner");
+const { DATABASE_PATH } = require("./constants");
 
 async function doStartup() {
+  await fsp.unlink(DATABASE_PATH);
   const database = await Database.construct();
   const parsedLibrary = await scanner.getLibrary();
   log.info("parsedLibrary", parsedLibrary);

63e804261c435c245c7b86ef977f84710cf778f3
Luis Prado <lpradopostigo@gmail.com>
2022-04-12 21:13:52 -0500

    remove initial scan in main process

--- src/main/services/controller.js
+++ src/main/services/controller.js
@@ -4,14 +4,6 @@ const Database = require("./Database");
 const scanner = require("./scanner");
 const { DATABASE_PATH } = require("./constants");
 
-async function doStartup() {
-  await fsp.unlink(DATABASE_PATH);
-  const database = await Database.construct();
-  const parsedLibrary = await scanner.getLibrary();
-  log.info("parsedLibrary", parsedLibrary);
-  await database.open();
-  await database.insertReleases(parsedLibrary);
-  await database.close();
-}
+async function doStartup() {}
 
 module.exports = { doStartup };

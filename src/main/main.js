e7b52ed2e02fdd50d0f0ce3dcb896f9b5e2673ea
Luis Prado <lpradopostigo@gmail.com>
2022-04-14 15:48:11 -0500

    cleaning, add conditional behavior on development

--- src/main/main.js
+++ src/main/main.js
@@ -1,22 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { app, BrowserWindow } = require("electron");
-const {
-  default: installExtension,
-  REACT_DEVELOPER_TOOLS,
-} = require("electron-devtools-installer");
 const path = require("path");
+const log = require("loglevel");
+
+log.setLevel("info");
+
 require("./handlers/library");
 require("./handlers/player");
 require("./handlers/window");
 require("./handlers/preferences");
-const log = require("loglevel");
-const controller = require("./services/controller");
-
-log.setLevel("info");
-
-controller.doStartup().then(() => {
-  log.info("startup complete");
-});
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -24,7 +16,6 @@ if (require("electron-squirrel-startup")) {
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
@@ -35,24 +26,26 @@ const createWindow = () => {
     titleBarStyle: "hidden",
   });
 
-  // and load the index.html of the app.
   // eslint-disable-next-line no-undef
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY).then(() => {
+    if (process.env.NODE_ENV === "development") {
+      mainWindow.webContents.openDevTools();
+    }
+  });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(async () => {
-  await installExtension(REACT_DEVELOPER_TOOLS);
+app.whenReady().then(() => {
+  if (process.env.NODE_ENV === "development") {
+    const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS,
+    } = require("electron-devtools-installer");
+    installExtension(REACT_DEVELOPER_TOOLS).then(() => {
+      log.info("chrome extensions successfully installed");
+    });
+  }
   createWindow();
 });
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
@@ -60,12 +53,7 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.

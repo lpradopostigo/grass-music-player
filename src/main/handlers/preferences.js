9c02c987aab3c3cbe6789e5f38b09a629929dcd7
Luis Prado <lpradopostigo@gmail.com>
2022-04-15 12:04:49 -0500

    require modules on demand

--- src/main/handlers/preferences.js
+++ src/main/handlers/preferences.js
@@ -1,16 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { ipcMain, dialog } = require("electron");
-const persistentStorage = require("../services/persistentStorage");
+const { ipcMain } = require("electron");
 
-ipcMain.handle("preferences:get-value", (_, key, defaultValue) =>
-  persistentStorage.getValue(key, defaultValue)
-);
+ipcMain.handle("preferences:get-value", (_, key, defaultValue) => {
+  const { getValue } = require("../services/persistentStorage");
+  return getValue(key, defaultValue);
+});
 
-ipcMain.handle("preferences:set-value", (_, key, value) =>
-  persistentStorage.setValue(key, value)
-);
+ipcMain.handle("preferences:set-value", (_, key, value) => {
+  const { setValue } = require("../services/persistentStorage");
+  return setValue(key, value);
+});
 
 ipcMain.handle("preferences:open-path-selector", async () => {
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  const { dialog } = require("electron");
   const paths = await dialog.showOpenDialog({
     properties: ["openDirectory", "createDirectory"],
   });
